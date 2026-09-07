7db6796de7dd52fd0f821ae7ea46d2af794a7818
Nehal Rahangdale <rahangdalenehal77@gmail.com>
2026-09-07 20:27:56 +0530

    Addded solution to add two number .

---
+++ LinkedList/AddTwoNumbersII.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+import java.util.Stack;
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        // Put l1 values into stack
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        // Put l2 values into stack
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        ListNode head = null;
+
+        // Add from right to left
+        while (!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
+
+            int sum = carry;
+
+            if (!s1.isEmpty()) {
+                sum += s1.pop();
+            }
+
+            if (!s2.isEmpty()) {
+                sum += s2.pop();
+            }
+
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            // Insert node at beginning
+            ListNode newNode = new ListNode(digit);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        return head;
+    }
+}
