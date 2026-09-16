8919507dfe2507cb05b5c3294e5573306d15f37b
Nehal Rahangdale <rahangdalenehal77@gmail.com>
2026-09-16 21:59:01 +0530

    Added solution of maximum product subarray.

---
+++ Arrays/MaximumProductSubarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int answer = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int current = nums[i];
+
+            // Negative number can turn minimum into maximum
+            if (current < 0) {
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
+            }
+
+            maxProduct = Math.max(current, maxProduct * current);
+            minProduct = Math.min(current, minProduct * current);
+
+            answer = Math.max(answer, maxProduct);
+        }
+
+        return answer;
+    }
+}
