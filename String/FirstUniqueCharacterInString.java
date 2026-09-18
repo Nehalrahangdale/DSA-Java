19a050c894713fa019fbf209dca4bb962a3b01bc
Nehal Rahangdale <rahangdalenehal77@gmail.com>
2026-09-18 22:38:32 +0530

    Added solution to find first unique character in a string.

---
+++ String/FirstUniqueCharacterInString.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstUniqChar(String s) {
+
+        int[] freq = new int[26];
+
+        // Step 1: Count frequency
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        // Step 2: Find first character with frequency 1
+        for (int i = 0; i < s.length(); i++) {
+
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
