5a51f80b941b7714efc6a1aa09332c18df1721b9
Nehal Rahangdale <rahangdalenehal77@gmail.com>
2026-09-20 18:07:17 +0530

    Added solution to find capacity to ship pakacage with D days.

---
+++ BinarySearch/CapacityToShipPackage.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public int shipWithinDays(int[] weights, int days) {
+
+        int left = 0;
+        int right = 0;
+
+        // Find minimum and maximum possible capacity
+        for (int weight : weights) {
+            left = Math.max(left, weight);
+            right += weight;
+        }
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int requiredDays = 1;
+            int currentWeight = 0;
+
+            // Calculate required days for this capacity
+            for (int weight : weights) {
+
+                if (currentWeight + weight > mid) {
+                    requiredDays++;
+                    currentWeight = 0;
+                }
+
+                currentWeight += weight;
+            }
+
+            if (requiredDays <= days) {
+                // Capacity works
+                // Try smaller capacity
+                right = mid - 1;
+            } else {
+                // Capacity is too small
+                // Need larger capacity
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
